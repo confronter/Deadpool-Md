e41e4bbf0963f637b0a306d52153bb33a54b1a01
confronter <142077542+confronter@users.noreply.github.com>
2024-07-06 18:04:29 +0300

    𝐃𝐞𝐚𝐝𝐩𝐨𝐨𝐥 𝐕2😍❗️

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-› King Sam
+› Confronter
 › 
                  >
 */
@@ -116,13 +116,13 @@ classic.sendMessage(anu.id, { image: { url: ppuser }, mentions: [num], caption: 
  ▬▭▬▭▬▭▬▭▬▬▭▬▭▬
 Terima Kasih Sudah Bergabung Jangan Lupa Baca Deskripsi Yah
 ▬▭▬▭▬▭▬▭▬▬▭▬▭▬
-Creator : https://wa.me/254742491666`})
+Creator : https://wa.me/254111766934`})
 } else if (anu.action == 'remove') {
 classic.sendMessage(anu.id, { image: { url: ppuser }, mentions: [num], caption: `Karena Untuk Setiap Ucapan Selamat Datang Akan Selalu Diakhiri Dengan Ucapan Selamat Tinggal 👋
 ▬▭▬▭▬▭▬▭▬▬▭▬▭▬
 Selamat Tinggal *@${num.split("@")[0]}* Di Group *${metadata.subject}*
 ▬▭▬▭▬▭▬▭▬▬▭▬▭▬
-Creator : https://wa.me/2547424916666`})
+Creator : https://wa.me/254111766934`})
 } else if (anu.action == 'promote') {
 classic.sendMessage(anu.id, { image: { url: ppuser }, mentions: [num], caption: `@${num.split('@')[0]} Ciee Jadi Admin Di Group ${metadata.subject} ${metadata.desc}`  })
 } else if (anu.action == 'demote') {
